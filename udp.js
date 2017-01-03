676ad714557148e6bb6041bd037219e76fe878af
nitaybz <nitaybz@gmail.com>
2017-01-03 17:10:04 +0200

    Update udp.js

--- udp.js
+++ udp.js
@@ -1,25 +1,34 @@
 module.exports = function (host, port, payload, callback) {
   var broadcast = '000000000000000009000000e00729070b00170a00000000c0a80a0555c100008ec20000000006000000000000000000'
-
-  var message = new Buffer(payload, 'hex');
-
   var dgram = require('dgram');
   var client = dgram.createSocket('udp4');
-  var delayTime = Math.floor(Math.random() * 1000) + 1;
+  
+  if (typeof payload === 'object') {
+      for (var i = 0; i < payload.length; i++){
+        sendPackets(payload[i]);
+      };
+  }
+  else {
+       sendPackets(payload);
+  };
+  
+  function sendPackets(payloadMessage){
+        var delayTime = Math.floor(Math.random() * 1000) + 1;
+        var message = new Buffer(payloadMessage, 'hex');
+        setTimeout(function() { 
+          client.send(broadcast, 0, broadcast.length, port, host, function(err, bytes) {
+            if (err) throw err;
+            console.log('UDP message sent to ' + host +':'+ port);
 
-  setTimeout(function() { 
-    client.send(broadcast, 0, broadcast.length, port, host, function(err, bytes) {
-      if (err) throw err;
-      console.log('UDP message sent to ' + host +':'+ port);
+            client.send(message, 0, message.length, port, host, function(err, bytes) {
+              if (err) throw err;
+              console.log('UDP message sent to ' + host +':'+ port);
+              client.close();
 
-      client.send(message, 0, message.length, port, host, function(err, bytes) {
-        if (err) throw err;
-        console.log('UDP message sent to ' + host +':'+ port);
-        client.close();
+              callback(err);
+            });
 
-        callback(err);
-      });
-
-    });
-  }, delayTime);
+          });
+        }, delayTime);
+    }
 }
